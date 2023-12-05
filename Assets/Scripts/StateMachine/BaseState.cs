0d5b32dd16e21b7c0e21f186a977a6dff7432025
Akash Sinha <66449020+theskysinha@users.noreply.github.com>
2023-12-06 00:56:26 +0530

    added base state and state manager

--- Assets/Scripts/StateMachine/BaseState.cs
+++ Assets/Scripts/StateMachine/BaseState.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class BaseState : MonoBehaviour
+public abstract class BaseState<EState> where EState : Enum
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public BaseState(EState key){
+        StateKey = key;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public EState StateKey { get; private set; }
+    public abstract void EnterState();
+    public abstract void ExitState();
+    public abstract void UpdateState();
+    public abstract EState GetNextState();
+    public abstract void OnTriggerEnter(Collider collider);
+    public abstract void OnTriggerStay(Collider collider);
+    public abstract void OnTriggerExit(Collider collider);
 }
--- Assets/Scripts/StateMachine/StateManager.cs
+++ Assets/Scripts/StateMachine/StateManager.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using System.Collections.Generic;
 
-public class StateManager : MonoBehaviour
+public abstract class StateManager<EState> : MonoBehaviour where EState : Enum
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    protected Dictionary<EState , BaseState<EState>> States = new Dictionary<EState, BaseState<EState>>();
+    protected BaseState<EState> CurrentState;
+    protected bool IsTransitioning = false;
+
+    void Start(){
+        CurrentState.EnterState();
+    }
+    void Update(){
+        EState nextStateKey = CurrentState.GetNextState();
+        if(!IsTransitioning && nextStateKey.Equals(CurrentState.StateKey)){
+            CurrentState.UpdateState();
+        } else if (!IsTransitioning) {
+            TransitionToState(nextStateKey);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void TransitionToState(EState nextStateKey){
+        IsTransitioning = true;
+        CurrentState.ExitState();
+        CurrentState = States[nextStateKey];
+        CurrentState.EnterState();
+        IsTransitioning = false;
+    }
+
+    void OnTriggerEnter(Collider other){
+        CurrentState.OnTriggerEnter(other);
+    }
+    void OnTriggerStay(Collider other){
+        CurrentState.OnTriggerStay(other);
+    }
+    void OnTriggerExit(Collider other){
+        CurrentState.OnTriggerExit(other);
     }
 }
